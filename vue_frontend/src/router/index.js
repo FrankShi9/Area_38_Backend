0d453b37f6195c2698b47f167f5ab3665b00c545
“Athens0” <zzl445862490@gmail.com>
2022-03-02 16:25:24 +0000

    front & back

---
+++ vue_frontend/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/components/Login.vue'
+//import Register from "@/components/Register";
+
+const routes = [
+  {
+    path: '/',
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+  // {
+  //   path: '/Register',
+  //   component: Register
+  // }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
